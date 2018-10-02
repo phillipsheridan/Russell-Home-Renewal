ba6eb8fe3f627c4f6aeb0a34f0ba8e6820f99d8d
Phillip <philsher93@gmail.com>
2018-10-02 17:27:58 -0400

    added build files so server can serve them

---
+++ rhr-app/build/precache-manifest.dbcfc61f030bb63c2e3442ef6335da4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8f7c8a66b8fba346ec0",
+    "url": "/static/js/main.d8f7c8a6.chunk.js"
+  },
+  {
+    "revision": "91ba1153c3d9e7e51c42",
+    "url": "/static/js/1.91ba1153.chunk.js"
+  },
+  {
+    "revision": "d8f7c8a66b8fba346ec0",
+    "url": "/static/css/main.24043c43.chunk.css"
+  },
+  {
+    "revision": "f0e943814ef0214ced13f2ab4776a667",
+    "url": "/index.html"
+  }
+];
